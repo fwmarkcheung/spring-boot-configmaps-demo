3505fef1d360e71b73be4543c19b1aeba875b85b
Mark Cheung <mcheung@redhat.com>
2021-11-22 15:28:19 -0500

    Changed to simply logging

--- src/main/java/com/redhat/developers/GreeterController.java
+++ src/main/java/com/redhat/developers/GreeterController.java
@@ -1,14 +1,18 @@
 package com.redhat.developers;
 
-import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 public class GreeterController {
+
+    private static final Logger LOGGER=LoggerFactory.getLogger(GreeterController.class);
 
     @Value("${greeter.message}")
     private String greeterMessageFormat;
@@ -20,15 +24,9 @@ public class GreeterController {
     public String greet(@PathVariable("user") String user) {
         String prefix = System.getenv().getOrDefault("GREETER_PREFIX", "Hi");
         String envProfile = System.getenv().getOrDefault("SRPING_PROFILES_ACTIVE", "default");
-        log.info("Prefix :{} and User:{}", prefix, user);
-        log.info("EnvProfile:{}", envProfile);
-        log.info("env :{}", env);
-
-
-
-//        if (prefix == null) {
-//            prefix = "Hello!";
-//        }
+        LOGGER.info("Prefix :{} and User:{}", prefix, user);
+        LOGGER.info("EnvProfile:{}", envProfile);
+        LOGGER.info("env :{}", env);
 
         return String.format(greeterMessageFormat, prefix, user, env);
     }
